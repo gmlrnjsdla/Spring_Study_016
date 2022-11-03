afcdb0b1603f6bbca8b7ed54d4ab35eee3845fcc
gmlrnjsdla <dlagmlrnjs@gmail.com>
2022-11-03 11:56:17 +0900

    트랜젝션 연습 // 적용전

---
+++ src/main/java/com/heekwoncompany/study016/TicketController.java
@@ -0,0 +1,34 @@
+package com.heekwoncompany.study016;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TicketController {
+
+	private TicketDao dao;
+
+	@Autowired
+	public void setDao(TicketDao dao) {
+		this.dao = dao;
+	}
+
+	@RequestMapping(value = "/buyTicket")
+	public String buyTicket() {
+		
+		return "buyTicket";
+	}
+	
+	@RequestMapping(value = "/buy_ticket_card")
+	public String buy_ticket_card(TicketDto ticketDto, Model model) {
+//		TicketDao dao = new TicketDao();
+		dao.buyTicket(ticketDto);
+		
+		model.addAttribute("ticketInfo", ticketDto);
+		
+		return "buy_ticket_end";
+	}
+	
+}
---
+++ src/main/java/com/heekwoncompany/study016/TicketDto.java
@@ -0,0 +1,27 @@
+package com.heekwoncompany.study016;
+
+public class TicketDto {
+	
+	private String consumerid;
+	private String amount;
+	
+
+	public String getConsumerid() {
+		return consumerid;
+	}
+
+	public void setConsumerid(String consumerid) {
+		this.consumerid = consumerid;
+	}
+
+	public String getAmount() {
+		return amount;
+	}
+
+	public void setAmount(String amount) {
+		this.amount = amount;
+	}
+	
+	
+	
+}
